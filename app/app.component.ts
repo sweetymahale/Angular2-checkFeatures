182adf2b965f9bd1a033185fb2992fe9637a8b73
sweetymahale <mahalesweety18@gmail.com>
2017-11-09 17:25:49 -0600

    added new way of changing the style

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector : 'my-app',
   template : `<div>
-              <div [ngClass]="{'my-class': changeStyle }"> On button click color will be changed to yellow </div>
-              <button (click)="changeStyle = !changeStyle;">Change Color to yellow</button>
-              <div [style.background-color]="getStyle()"> On button click color will be changed to Red </div>
-              <button (click)="changeStyleRed = !changeStyleRed;">Change Color to red</button>
+                <div [ngClass]="{'my-class': changeStyle }"> On button click color will be changed to yellow </div>
+                <button (click)="changeStyle = !changeStyle;">Change Color to yellow</button>
+
+                <div [class.my-class-changebg]="changeStyleBlue"> On button click color will be changed to blue.... </div>
+                <button (click)="changeStyleBlue = !changeStyleBlue;">Change Color to Blue</button>
+
+                <div [style.background-color]="getStyle()"> On button click color will be changed to Red </div>
+                <button (click)="changeStyleRed = !changeStyleRed;">Change Color to red</button>
+                <div styled> Change the Style    </div>
 
               </div>`,
   styles: [
@@ -14,16 +20,21 @@ import { Component } from '@angular/core';
   .my-class {
     background-color: yellow;
   }
+  .my-class-changebg {
+    background-color: blue;
+  }
   `
   ]
 })
 
 export class AppComponent {
   changeStyle = false;
+  changeStyleRed = false;
+  changeStyleBlue = false;
 
   constructor() {
  }
- changeStyleRed = false;
+
 
  getStyle() {
      if(this.changeStyleRed) {
